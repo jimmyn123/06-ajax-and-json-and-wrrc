49e76ecc5732d4c2b40bb381047921cbe22b5daf
Jeff <jhwe92@gmail.com>
2018-09-11 10:06:26 -0700

    Created AJAX request for data population when localStorage is not present, and stores them. Finished comments

--- jeff-jimmy/scripts/article.js
+++ jeff-jimmy/scripts/article.js
@@ -12,16 +12,16 @@ function Article (rawDataObj) {
 // REVIEW: Instead of a global `articles = []` array, let's attach this list of all articles directly to the constructor function. Note: it is NOT on the prototype. In JavaScript, functions are themselves objects, which means we can add properties/values to them at any time. In this case, the array relates to ALL of the Article objects, so it does not belong on the prototype, as that would only be relevant to a single instantiated Article.
 Article.all = [];
 
-// COMMENT: Why isn't this method written as an arrow function?
-// PUT YOUR RESPONSE HERE
+// DONE** COMMENT: Why isn't this method written as an arrow function?
+// Since we are using contextual 'this' for this method to refer to this instance of Article, we cannot use arrow functions as they do not pass in contextual 'this'.
 Article.prototype.toHtml = function() {
   let template = Handlebars.compile($('#article-template').text());
 
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
 
-  // COMMENT: What is going on in the line below? What do the question mark and colon represent? How have we seen this same logic represented previously?
+  // DONE** COMMENT: What is going on in the line below? What do the question mark and colon represent? How have we seen this same logic represented previously?
   // Not sure? Check the docs!
-  // PUT YOUR RESPONSE HERE
+  // We used an if else before. A ternary is going on below. publishStatus is checking the truthy-ness of publishedOn. If it's truthy, then store the first value. If not, then '(draft)' is stored into the property.
   this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
   this.body = marked(this.body);
 
@@ -32,12 +32,12 @@ Article.prototype.toHtml = function() {
 
 // REVIEW: This function will take the rawData, how ever it is provided, and use it to instantiate all the articles. This code is moved from elsewhere, and encapsulated in a simply-named function for clarity.
 
-// COMMENT: Where is this function called? What does 'rawData' represent now? How is this different from previous labs?
-// PUT YOUR RESPONSE HERE
+// DONE** COMMENT: Where is this function called? What does 'rawData' represent now? How is this different from previous labs?
+// It is called in Article.fetchAll. rawData represents our local data that's been stored from 'server data' (hackerIpsum.json).
 Article.loadAll = articleData => {
-  articleData.sort((a,b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)))
+  articleData.sort((a,b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)));
 
-  articleData.forEach(articleObject => Article.all.push(new Article(articleObject)))
+  articleData.forEach(articleObject => Article.all.push(new Article(articleObject)));
 }
 
 // REVIEW: This function will retrieve the data from either a local or remote source, and process it, then hand off control to the View.
@@ -45,9 +45,17 @@ Article.fetchAll = () => {
   // REVIEW: What is this 'if' statement checking for? Where was the rawData set to local storage?
   if (localStorage.rawData) {
 
-    Article.loadAll();
+    Article.loadAll(JSON.parse(localStorage.rawData));
 
   } else {
-
+    $.ajax({
+      url: '../data/hackerIpsum.json',
+      method: 'GET',
+      success: (data) => {
+        let rawData = JSON.stringify(data);
+        localStorage.setItem('rawData', rawData);
+        Article.loadAll(data);
+      }
+    })
   }
 }
